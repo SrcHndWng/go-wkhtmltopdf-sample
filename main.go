55fb83b958a116af2e942e8f00c59d6ad5953d37
undersourcecode <undersourcecode@hotmail.co.jp>
2018-12-09 11:16:23 +0900

    出力を表形式に変更。

--- main.go
+++ main.go
@@ -12,17 +12,44 @@ import (
 func main() {
 	const html = `<!doctype html>
 	<style>
-		.line {width:100px; border:#000000 solid 1px}
+	.table {
+		border-collapse:collapse;
+		border-spacing:0px; 
+		border:1px solid #FF0000;
+	}
+
+	.table tr td:nth-child(1) {
+		width:300px;
+	}
+
+	.table tr td {
+		border:1px solid #000000;
+	}
 	</style>
 	<html>
-			<head>
-				<title>PRINT PDF TEST</title>
-				<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-			</head>
+		<head>
+			<title>PRINT PDF TEST</title>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		</head>
 		<body>
-			<div class="line">あああ</div>
-			<div class="line">いいい</div>
-			<div class="line">ううう</div>
+		<table class="table">
+			<tr>
+				<td>品名</td>
+				<td>口径</td>
+			</tr>
+			<tr>
+				<td>M16</td>
+				<td>5.56</td>
+			</tr>
+			<tr>
+				<td>AK47</td>
+				<td>7.72</td>
+			</tr>
+			<tr>
+				<td>MP5</td>
+				<td>9.00</td>
+			</tr>
+	  </table>
 		</body>
 	</html>`
 
